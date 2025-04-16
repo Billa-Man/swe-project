40045a1f0f9e08e76465d0bebdd0a2b2550299e0
Sohith Bandari <sohith.bandari@gmail.com>
2025-04-15 21:45:12 -0400

    this works

--- phishlearn/core/urls.py
+++ phishlearn/core/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('course/<int:course_id>/', views.course_detail, name='course_detail'),
     path('quiz/<int:quiz_id>/', views.take_quiz, name='take_quiz'),
-    # path('send-phishing-test/', views.send_phishing_test, name='send_phishing_test'),
     path('manage-employees/', views.list_employees, name='manage_employees'),
     path('manage-courses/', views.manage_courses, name='manage_courses'),
     path('manage-templates/', views.manage_templates, name='manage_templates'),
